37b01c984eef130742130ce87d1343b0bfe1cb6b
Shivansh Gupta <shivanshgupta290804@gmail.com>
2025-04-13 11:21:57 +0530

    Testing CI/CD

--- Bot/views.py
+++ Bot/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 import requests
 from rest_framework.decorators import api_view,permission_classes
-from rest_framework.exceptions import Throttled
 # Create your views here.
 
 @api_view(['POST'])
